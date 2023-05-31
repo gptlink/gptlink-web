6c4ad71d5b217484422d78e94d236910a952f1ca
pengYYY <pengyue970715@gmail.com>
2023-06-01 00:43:47 +0800

    feat: add commitlint and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',"tailwindcss"],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
