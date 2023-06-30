3c142a0e1660a234db058818f7ad73f4c030e302
PY <pengyue970715@gmail.com>
2023-06-30 23:37:16 +0800

    Revert "[WIP]feat: 增加消息导出图片 (#4)" (#5)
    This reverts commit f44608604a6cdc680bccc91bd1075e532bc007f1.

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,13 +1,11 @@
 import { useState, useRef, useEffect } from 'react';
-import { Loader2, PauseOctagon, SendIcon, Trash2Icon, DownloadIcon } from 'lucide-react';
+import { Loader2, PauseOctagon, SendIcon, Trash2Icon } from 'lucide-react';
 
 import { useChatStore } from '@/store';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import IconSvg from '@/components/Icon';
-import { Checkbox } from '@/components/ui/checkbox';
-import MessageExporter from './MessageExporter';
 
 import { ChatItem } from './ChatItem';
 import classNames from 'classnames';
@@ -15,13 +13,11 @@ import classNames from 'classnames';
 let scrollIntoViewTimeId = -1;
 const Footer = () => {
   const [userInput, setUserInput] = useState('');
-  const [isDownload, setIsDownload] = useState(false);
-  const [sendUserMessage, isStream, clearCurrentConversation, stopStream, currentChatData] = useChatStore((state) => [
+  const [sendUserMessage, isStream, clearCurrentConversation, stopStream] = useChatStore((state) => [
     state.sendUserMessage,
     state.isStream,
     state.clearCurrentConversation,
     state.stopStream,
-    state.currentChatData(),
   ]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -49,84 +45,48 @@ const Footer = () => {
   }, [textAreaRef, userInput]);
 
   return (
-    <footer
-      className={classNames('flex items-end gap-4 p-4', {
-        'border-t items-center justify-between': isDownload,
-      })}
-    >
-      {!isDownload ? (
-        <>
-          <div className="flex gap-1">
-            <Button
-              variant={'ghost'}
-              className="mb-1 h-9 w-9 shrink-0 rounded-full p-0"
-              disabled={isStream}
-              onClick={() => {
-                if (confirm('你确定要清除所有的消息吗？')) {
-                  clearCurrentConversation();
-                  setUserInput('');
-                }
-              }}
-            >
-              <Trash2Icon size={16} />
-            </Button>
-            <Button
-              variant={'ghost'}
-              className="mb-1 h-9 w-9 shrink-0 rounded-full p-0"
-              disabled={isStream}
-              onClick={() => {
-                setIsDownload(true);
-              }}
-            >
-              <DownloadIcon size={16} />
+    <footer className="flex items-end gap-4 p-4">
+      <Button
+        variant={'ghost'}
+        className="mb-1 h-9 w-9 shrink-0 rounded-full p-0"
+        disabled={isStream}
+        onClick={() => {
+          if (confirm('你确定要清除所有的消息吗？')) {
+            clearCurrentConversation();
+            setUserInput('');
+          }
+        }}
+      >
+        <Trash2Icon size={16} />
+      </Button>
+      <div className="relative flex-1">
+        {isStream && (
+          <div className="absolute left-0 z-10 flex w-full justify-center">
+            <Button variant="destructive" onClick={() => stopStream()}>
+              <PauseOctagon></PauseOctagon>
             </Button>
           </div>
-          <div className="relative flex-1">
-            {isStream && (
-              <div className="absolute left-0 z-10 flex w-full justify-center">
-                <Button variant="destructive" onClick={() => stopStream()}>
-                  <PauseOctagon></PauseOctagon>
-                </Button>
-              </div>
-            )}
-            <Textarea
-              ref={textAreaRef}
-              className={classNames('h-10 max-h-[7rem] min-h-[40px] w-full flex-1 resize-none scroll-bar-none', {
-                'blur-sm': isStream,
-              })}
-              onKeyDown={handleKeyDown}
-              disabled={isStream}
-              value={userInput}
-              placeholder="来说点什么...（Shift + Enter = 换行）"
-              onChange={(val) => setUserInput(val.target.value)}
-            />
-          </div>
-          <Button
-            disabled={isStream || !userInput.replace(/\n/g, '')}
-            onClick={() => {
-              handleSendUserMessage();
-            }}
-          >
-            {!isStream ? <SendIcon /> : <Loader2 className="m-auto my-32 animate-spin" />}
-          </Button>
-        </>
-      ) : (
-        <>
-          <div className="flex items-center">
-            <Checkbox id="terms" className="mr-2" />
-            全选
-          </div>
-          <MessageExporter messages={currentChatData} />
-          <Button
-            variant={'destructive'}
-            onClick={() => {
-              setIsDownload(false);
-            }}
-          >
-            取消
-          </Button>
-        </>
-      )}
+        )}
+        <Textarea
+          ref={textAreaRef}
+          className={classNames('h-10 max-h-[7rem] min-h-[40px] w-full flex-1 resize-none scroll-bar-none', {
+            'blur-sm': isStream,
+          })}
+          onKeyDown={handleKeyDown}
+          disabled={isStream}
+          value={userInput}
+          placeholder="来说点什么...（Shift + Enter = 换行）"
+          onChange={(val) => setUserInput(val.target.value)}
+        />
+      </div>
+      <Button
+        disabled={isStream || !userInput.replace(/\n/g, '')}
+        onClick={() => {
+          handleSendUserMessage();
+        }}
+      >
+        {!isStream ? <SendIcon /> : <Loader2 className="m-auto my-32 animate-spin" />}
+      </Button>
     </footer>
   );
 };
