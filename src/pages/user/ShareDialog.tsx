c6113589616bacf0a14af6efa0a1915d5da9f9d8
pengYYY <pengyue970715@gmail.com>
2023-07-02 00:16:54 +0800

    feat: poster optimize

--- src/pages/user/ShareDialog.tsx
+++ src/pages/user/ShareDialog.tsx
@@ -84,7 +84,7 @@ export function ShareDialog({ open, shareUrl, handleOpenChange }: ShareDialogPro
               </Button>
             </div>
             <ScrollArea>
-              <AlertDialogDescription className="h-[35rem]">
+              <AlertDialogDescription className="h-[35rem] max-sm:h-[30rem]">
                 <img src={dataUrl} className="w-full" alt="" />
               </AlertDialogDescription>
             </ScrollArea>
