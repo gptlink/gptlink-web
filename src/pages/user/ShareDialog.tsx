31500a2ddd2b2cbd7d13f071b92628177778efe5
pengYYY <pengyue970715@gmail.com>
2023-07-07 12:00:11 +0800

    fix: poster generate error

--- src/pages/user/ShareDialog.tsx
+++ src/pages/user/ShareDialog.tsx
@@ -1,28 +1,13 @@
-import { useRef, useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
+import { useRef, useEffect } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 import { toast } from 'react-hot-toast';
 import { toJpeg } from 'html-to-image';
-import { saveAs } from 'file-saver';
-import { Loader2 } from 'lucide-react';
-import { useCopyToClipboard } from 'usehooks-ts';
 
-import { ScrollArea } from '@/components/ui/scroll-area';
 import poster from '@/assets/poster.png';
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogAction,
-  AlertDialogFooter,
-  AlertDialogCancel,
-} from '@/components/ui/alert-dialog';
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { useMobileScreen } from '@/hooks/use-mobile-screen';
-import useTask from '@/hooks/use-task';
+import { useCopyToClipboard } from 'usehooks-ts';
 
 type ShareDialogProps = {
   open: boolean;
@@ -31,106 +16,59 @@ type ShareDialogProps = {
 };
 
 export function ShareDialog({ open, shareUrl, handleOpenChange }: ShareDialogProps) {
+  const canvasRef = useRef<HTMLImageElement>(null);
   const posterRef = useRef<HTMLDivElement>(null);
   const [, copy] = useCopyToClipboard();
-  const isMobileScreen = useMobileScreen();
-  const [dataUrl, setDataUrl] = useState('');
-  const [loading, setLoading] = useState(false);
-  const { shareCallback } = useTask();
 
   const drawImage = async (element: HTMLElement) => {
-    const res = await toJpeg(element, { style: { opacity: '1' } });
-    setDataUrl(res);
+    const dataUrl = await toJpeg(element, { style: { opacity: '1' } });
+    if (!canvasRef.current) return;
+    canvasRef.current.src = dataUrl;
   };
 
   useEffect(() => {
     if (open) {
-      setLoading(true);
       setTimeout(() => {
-        setLoading(false);
         if (!posterRef.current) return;
         drawImage(posterRef.current);
-      }, 200);
+      }, 300);
     }
   }, [open]);
 
   return (
-    <>
-      {open &&
-        createPortal(
-          <div ref={posterRef} className="relative w-[25rem]">
-            <img src={poster} className="w-full" />
-            <QRCodeCanvas
-              className="absolute bottom-2 left-[50%] m-auto -translate-x-1/2"
-              style={{
-                width: '3.5rem',
-                height: '3.5rem',
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogContent className="w-[25rem]">
+        <DialogTitle>分享</DialogTitle>
+        <div>
+          <div className="mb-2 flex">
+            <Input value={shareUrl} readOnly></Input>
+            <Button
+              className="ml-2 shrink-0"
+              onClick={() => {
+                copy(shareUrl);
+                toast.success('复制成功');
               }}
-              value={shareUrl}
-            />
-          </div>,
-          document.body,
-        )}
-      <AlertDialog
-        open={open}
-        onOpenChange={(val) => {
-          if (!val) {
-            shareCallback();
-          }
-          handleOpenChange(val);
-        }}
-      >
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>分享海报</AlertDialogTitle>
-          </AlertDialogHeader>
-          <div className="w-full overflow-hidden">
-            <div className="mb-2 flex">
-              <Input value={shareUrl} readOnly></Input>
-              <Button
-                className="ml-2 shrink-0"
-                onClick={() => {
-                  try {
-                    copy(shareUrl);
-                    toast.success('复制成功');
-                  } catch {
-                    toast.error('复制失败');
-                  }
-                }}
-              >
-                复制链接
-              </Button>
-            </div>
-            <ScrollArea>
-              <div className="h-[35rem] max-sm:h-[25rem]">
-                {loading ? (
-                  <div className="p-48">
-                    <Loader2 className="m-auto animate-spin" />
-                  </div>
-                ) : (
-                  <img src={dataUrl} className="w-full" alt="" />
-                )}
-              </div>
-            </ScrollArea>
-
-            <AlertDialogDescription className="mt-2 text-center">
-              {isMobileScreen && '长按'}保存上方图片，分享你专属海报给朋友
-            </AlertDialogDescription>
+            >
+              复制链接
+            </Button>
           </div>
-          <AlertDialogFooter>
-            <AlertDialogCancel>取消</AlertDialogCancel>
-            {!isMobileScreen && (
-              <AlertDialogAction
-                onClick={() => {
-                  saveAs(dataUrl, '分享海报.jpg');
+          <div className="relative">
+            <img ref={canvasRef} src="" className="absolute left-0 top-0 w-full" />
+            <div ref={posterRef}>
+              <img src={poster} className="w-full" />
+              <QRCodeCanvas
+                className="absolute bottom-1 left-[50%] m-auto -translate-x-1/2"
+                style={{
+                  width: '3.5rem',
+                  height: '3.5rem',
                 }}
-              >
-                下载
-              </AlertDialogAction>
-            )}
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </>
+                value={shareUrl}
+              />
+            </div>
+          </div>
+          <p className="mt-4 text-center">保存上方图片，分享你专属海报给朋友</p>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
