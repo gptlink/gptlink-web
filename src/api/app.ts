9a06d4ce0a93d2b5952726b78b65e4f29efab64b
PY <pengyue970715@gmail.com>
2023-07-13 14:08:14 +0800

    feat: add login-type api (#12)
    * feat: add login-type api
    * perf: 移除默认站点名称

--- src/api/app.ts
+++ src/api/app.ts
@@ -14,14 +14,20 @@ export enum LoginTypeEnum {
   WECHAT_AND_PHONE = '4',
 }
 
-export interface AppConfigType {
+export interface AppConfigInterface {
   name: string;
   icp: string;
   web_logo: string;
   admin_logo: string;
-  login_type: LoginTypeEnum;
   user_logo: string;
 }
+
+export interface LoginTypeInterface {
+  login_type: LoginTypeEnum;
+  mobile_verify: boolean;
+}
+
+export interface AppConfigType extends LoginTypeInterface, AppConfigInterface {}
 
 export interface JsSDKType {
   debug: boolean;
@@ -46,8 +52,11 @@ export default {
   getConfigAgreement(): Promise<ConfigAgreementType> {
     return request('config/agreement');
   },
-  getAppConfig(): Promise<AppConfigType> {
+  getAppConfig(): Promise<AppConfigInterface> {
     return request('config/basic-info');
+  },
+  getLoginType(): Promise<LoginTypeInterface> {
+    return request('config/login-type');
   },
   getJsSDK(url: string): Promise<{
     data: JsSDKType;
