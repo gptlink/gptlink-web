5174be5b3760e5797cb80e3b0bb107fc6cdef2c6
pengYYY <pengyue970715@gmail.com>
2023-07-26 10:32:03 +0800

    fix: chat progress error message

--- src/utils/stream-api.ts
+++ src/utils/stream-api.ts
@@ -93,9 +93,13 @@ export default class StreamAPI {
 
       try {
         const dataList = chunkValue.split('\n\ndata :');
-        const chunk = dataList[dataList.length - 2];
+        const chunk = dataList[dataList.length - 2] || dataList[dataList.length - 1];
         if (chunk) {
           resChunkValue = JSON.parse(chunk);
+          if (resChunkValue.err_code > 0) {
+            onError(resChunkValue.err_msg);
+            return;
+          }
           onProgress(resChunkValue);
         }
       } catch (e) {
